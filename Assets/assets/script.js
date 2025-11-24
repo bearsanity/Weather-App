0154d0ebc7ef3b8af8cf83815188af959fec2491
bearsanity <jsweeney96@hotmail.com>
2025-11-24 20:08:04 -0330

    Added renderCurrentWeather function

--- Assets/assets/script.js
+++ Assets/assets/script.js
@@ -7,9 +7,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const searchButton = $('#search-button');
     const cityInput = $('#city-input');
     
-    //APIs
+    //APIs - URLs aren't called here, this is just to have them in one place
     const apiKey = 'e26922b5bf8ae6d46c0de5695047bfd2';
-    //const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`; -> Moved inside function because the city changes with every search
+    //const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
+    //const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
+
 
     //================
     //Helper Functions
@@ -17,12 +19,52 @@ document.addEventListener('DOMContentLoaded', () => {
     
     //To be called in getCoordinates to get weather once coordinates have been found
     async function getWeather(latitude, longitude) {
-        const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+        const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
 
         const response = await fetch(weatherUrl);
         const data = await response.json();
 
         console.log('Weather data:', data);
+        renderCurrentWeather(data);
+    }
+    
+    //Creates the card for current weather
+    function renderCurrentWeather(weatherInfo) {
+        const container = $('#current-weather');
+        container.empty(); //To clear out old content if nto already empty
+        
+        const current = weatherInfo.list[0] //cleaner than using weatherInfo.list[0] over and over
+        
+        const city = weatherInfo.city.name;
+        const temp = (current.main.temp).toFixed(1);
+        const humidity = (current.main.humidity).toFixed(0);
+        const wind = (current.wind.speed).toFixed(2);
+        const icon = current.weather[0].icon; //gives the icon code
+       
+        //Javascript Date object to convert the dt from the api
+        const date = new Date(current.dt * 1000).toLocaleDateString("en-US", {
+            month: "short", //Shows abbreviation for the month name so it can be quickly understood anywhere on the planet in cases where day is <13
+            day: "numeric",
+            year: "numeric"
+        });
+
+        //Creating DOM elements
+        const header = $('<h2>').text(`${city} - ${date}`);
+        const img = $('<img>')
+            .attr('src', `https://openweathermap.org/img/wn/${icon}@2x.png`) //as per the docs, this will take in the icon code (decalred as icon earlier) and display the correct icon
+            .attr('alt', 'Icon displaying the current weather');
+        const tempEl = $('<p>').text(`Temperature: ${temp}\u00B0C`);
+        const humidityEl = $('<p>').text(`Humidity: ${humidity}\u0025`);
+        const windEl = $('<p>').text(`Wind speed: ${wind}m\u002Fs`);
+
+        container.append(
+            header,
+            img,
+            tempEl,
+            humidityEl,
+            windEl
+        )
+
     }
     
     //====================
@@ -44,9 +86,10 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log('Lat:', latitude);
         console.log('Long:', longitude);
 
-        const weather = getWeather(latitude, longitude);
-        console.log('weather:', weather); //console.log to see that it's working
+        getWeather(latitude, longitude); //getWeather already ends with a console.log
     }
+
+
 
     //===============
     //Event listeners
