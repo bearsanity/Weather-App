8548eb8191c834113591d95644373e6524a52b47
bearsanity <jsweeney96@hotmail.com>
2025-11-24 03:19:16 -0330

    added get weather function

--- Assets/assets/script.js
+++ Assets/assets/script.js
@@ -11,13 +11,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     //Functions
+    //To be called in getCoordinates to get weather once coordinates have been found
+    async function getWeather(latitude, longitude) {
+        const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+
+        const response = await fetch(weatherUrl);
+        const data = await response.json();
+
+        console.log('Weather data:', data);
+    }
+    
+    //Gets coordinates tthen plugs them into get weather function to get weather
     async function getCoordinates(city){
-        const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
+        const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
 
         const response = await fetch(geoUrl);
         const data = await response.json();
         //console.log to see that it's working
-        console.log('Data', data);
+        console.log('Data:', data);
         
         //limit = 1 in the api means only 1 result is returned, but it always returns an array so data[0] is neccesary
         const latitude = data[0].lat;
@@ -26,8 +37,10 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log('Lat:', latitude);
         console.log('Long:', longitude);
 
+        const weather = getWeather(latitude, longitude);
+        console.log('weather:', weather);
     }
-    
+
     //Event listeners
     searchButton.on('click', () => {
         const city = cityInput.val().trim();
